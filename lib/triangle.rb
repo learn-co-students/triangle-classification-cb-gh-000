e324ac3f4a3b10b72e015ebbbd3966ae37c86d01
Federico Gobbo <federico.gobbo.2@studenti.unipd.it>
2017-04-09 21:29:56 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+  attr_accessor :first_side, :second_side, :third_side
+
+  def initialize(first_side, second_side, third_side)
+    @first_side = first_side
+    @second_side = second_side
+    @third_side = third_side
+    raise TriangleError unless valid?
+  end
+
+  def valid?
+    first_side + second_side > third_side &&
+      first_side + third_side > second_side &&
+      second_side + third_side > first_side &&
+      first_side > 0 && second_side > 0 && third_side > 0
+  end
+
+  def kind
+    if first_side == second_side && second_side == third_side
+      :equilateral
+    elsif first_side == second_side && second_side != third_side ||
+      first_side == third_side && second_side != third_side ||
+      second_side == third_side && first_side != second_side
+      :isosceles
+    else
+      :scalene
+    end
+  end
+end
+
+class TriangleError < StandardError
+
 end
