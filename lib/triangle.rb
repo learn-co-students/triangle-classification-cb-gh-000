b41400591a08b7bd5879217b5a300fe07a6563ca
Tuguldur Baigalmaa <philoniare@gmail.com>
2017-06-02 15:45:40 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+  attr_reader :kind
+
+  def initialize(x, y, z)
+    if !self.valid?(x, y, z)
+      raise TriangleError
+    end
+    @kind = self.determine_kind(x, y, z)
+  end
+  def determine_kind(x, y, z)
+    if x == y && y == z
+      :equilateral
+    elsif x == y || y == z || x == z
+      :isosceles
+    else
+      :scalene
+    end
+  end
+  def valid?(x, y, z)
+    [x, y, z].all?{|num| num > 0} && !([x + y > z, y + z > x, x + z > y].include?(false))
+  end
+end
+class TriangleError < StandardError
+  def message
+
+  end
 end
