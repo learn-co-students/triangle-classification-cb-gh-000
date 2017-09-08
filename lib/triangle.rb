812f8448b06bf4d0bea25eeb5338b10786883dc5
Hayden Betts <haydenbetts@gmail.com>
2017-09-08 18:43:40 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+  # the sum of any two sides of a triangle must always exceed the lenghht of the
+  # third side. Each side must also be larger than 0.
+
+  def initialize(length_one,length_two,length_three)
+    @l_1 = length_one
+    @l_2 = length_two
+    @l_3 = length_three
+  end
+
+  # returns :equilateral, :isosceles, or :scalene
+  # raises a TriangleError if the triangle is invalid
+  def kind
+    if (@l_1 + @l_2 <= @l_3) || (@l_2 + @l_3 <= @l_1) || (@l_1 + @l_3 <= @l_2)
+      raise TriangleError
+    elsif @l_1 <= 0 || @l_2 <= 0 || @l_3 <= 0
+      raise TriangleError
+    elsif @l_1 == @l_2 && @l_2 == @l_3
+      :equilateral
+    elsif @l_1 != @l_2 && @l_2 != @l_3 && @l_1 != @l_3
+      :scalene
+    else
+      :isosceles
+    end
+  end
+
+end
+
+class TriangleError < StandardError
 end
