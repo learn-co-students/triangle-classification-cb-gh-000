9a2b965dfa87eea3f35563781769d8e205a9ba79
Roberto Garza <robertg4567@hotmail.com>
2018-08-07 19:35:37 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    triangleChecker
+    if @a == @b and @a == @c
+      :equilateral
+    elsif @a == @b || @a == @c || @b == @c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def triangleChecker
+    if @a <= 0 || @b <= 0 || @c <= 0 || @a + @b <= @c || @a + @c <= @b || @b + @c <= @a
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
