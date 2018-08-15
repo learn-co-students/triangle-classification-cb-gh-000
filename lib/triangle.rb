3e2b84c72e61758b33eea6435bb940f161c0cd45
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-15 20:00:22 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,6 +8,12 @@ class Triangle
   end
   
   def kind
+    if ((@length_side1 + @length_side2) <= @length_side3 || (@length_side1 + @length_side3) <= @length_side2 || (@length_side3 + @length_side2) <= @length_side1)
+      raise TriangleError
+    end
+    if @length_side1 == 0 && @length_side2 == 0 && @length_side3 == 0 
+      raise TriangleError
+    end
     if @length_side1 == @length_side2 && @length_side1 == @length_side3 && @length_side2 == @length_side3
       return :equilateral
     elsif @length_side1 == @length_side2 || @length_side1 == @length_side3 || @length_side3 == @length_side2
@@ -15,21 +21,11 @@ class Triangle
     else
       return :scalene
     end
-    
-    # if @length_side1 == 0 && @length_side2 == 0 && @length_side3 == 0
-    #   begin
-    #     raise TriangleError
-    #     rescue TriangleError => error 
-    #       puts error.message
-    #     end
-    # end
-    
-    
   end
 
   class TriangleError < StandardError
-    def message 
-      "you must give the get_married method an argument of an instance of the person class!"
-    end
+    # def message 
+    #   "you must give the get_married method an argument of an instance of the person class!"
+    # end
   end
 end
