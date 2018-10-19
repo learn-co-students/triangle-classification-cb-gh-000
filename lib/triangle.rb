11606f97a4ed6aafedac89d340a7f7391896b0c3
John Casabar <johncasabar@gmail.com>
2018-10-19 08:18:04 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,41 @@
 class Triangle
-  # write code here
+  def initialize(side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
+  end
+
+  def error_handle
+    begin
+      raise TriangleError
+#    rescue TriangleError => error
+#        puts error.message
+    end
+  end
+
+  def kind
+    triangle_array = [@side_one, @side_two, @side_three].sort
+
+    if triangle_array == [0, 0, 0]
+      error_handle
+    elsif triangle_array.any? { |e| e < 0  }
+      error_handle
+    elsif triangle_array[0] + triangle_array[1] <= triangle_array[2]
+      error_handle
+    else
+      if triangle_array.all? { |e| e == triangle_array[0] }
+        :equilateral
+      elsif triangle_array[0] == triangle_array[1] || triangle_array[1] == triangle_array[2]
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Illegal triangle length combination"
+    end
+  end
 end
