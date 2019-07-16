cf6dbb574eb5031ab7da5248039e258866484e10
Danish Naseem <danishnaseem05@gmail.com>
2019-07-16 02:15:42 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
+require 'pry'
 class Triangle
   # write code here
+  attr_reader :length1, :length2, :length3
+
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end
+
+  def kind
+    if ((length1+length2 <= length3) || (length2+length3 <= length1) || (length1+length3 <= length2)) || (length1 <= 0 || length2 <= 0 || length3 <= 0)
+      raise TriangleError
+      # begin
+      #   raise TriangleError
+      # rescue TriangleError => error
+      #   puts error.message
+      # end
+    else
+      if length1==length2 && length2==length3 && length1==length3
+        :equilateral
+      elsif length1==length2 || length2==length3 || length1==length3
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "invalid triangle"
+    end
+  end
 end
